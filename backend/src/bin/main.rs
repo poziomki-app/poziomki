3ec71584ff14e88a06db704794aa4d4db8ff90c2
Dawid Piotrkowski <199792507+dpiotrkowski@users.noreply.github.com>
2026-04-16 22:04:41 +0200

    Switch backend runtime to distroless (#151)

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,5 +1,8 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    if std::env::args().nth(1).as_deref() == Some("healthcheck") {
+        poziomki_backend::healthcheck::api_healthcheck().await;
+    }
     poziomki_backend::app::run_api_server()
         .await
         .map_err(Into::into)
